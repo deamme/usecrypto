9928262f779d6e84bc45bb9aa80e9a07e7368ea6
Frederik Bolding <frederik.bolding@gmail.com>
2019-10-09 23:33:38 +0200

    Sorted entries alphabetically

--- src/components/EntryList/index.js
+++ src/components/EntryList/index.js
@@ -4,9 +4,11 @@ import Entry from "./Entry"
 export default function EntryList({ entries }) {
   return (
     <div className="list">
-      {entries.map(e => (
-        <Entry entry={e} />
-      ))}
+      {entries
+        .sort((a, b) => a.title.localeCompare(b.title))
+        .map((e, index) => (
+          <Entry key={index} entry={e} />
+        ))}
     </div>
   )
 }
