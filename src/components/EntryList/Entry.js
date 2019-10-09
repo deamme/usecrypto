721f61adf0eb409bdebb560bcc7397c79f0a0a74
deam <deamme@users.noreply.github.com>
2019-10-09 23:41:19 +0200

    Make entries clickable as links

--- src/components/EntryList/Entry.js
+++ src/components/EntryList/Entry.js
@@ -1,5 +1,9 @@
 import React from "react"
 
 export default function Entry({ entry }) {
-  return <div className="entry">{entry.title}</div>
+  return (
+    <a className="entry" href={entry.link} target="_blank">
+      {entry.title}
+    </a>
+  )
 }
