7523f14a0f042fda271e95da0817be1975e90e39
Frederik Bolding <frederik.bolding@gmail.com>
2019-10-10 00:19:56 +0200

    Fixed warning

--- src/components/EntryList/Entry.js
+++ src/components/EntryList/Entry.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Entry({ entry }) {
   return (
-    <a className="entry" href={entry.link} target="_blank">
+    <a className="entry" href={entry.link} target="_blank" rel="noopener noreferrer">
       {entry.title}
     </a>
   )
