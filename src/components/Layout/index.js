b2927aae37081a98fa3c82ba457f5bc8491f290d
Andreas Daskalopoulos <mcaibad2@gmail.com>
2019-10-12 17:57:04 +0300

    Add Github badge (#12)
    * Add Github badge #11: Added a GitHub commit activity badge at the top right corner

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,6 @@
 import React from "react"
+import GithubCorner from "react-github-corner"
+
 import { Helmet } from "react-helmet"
 
 import "./assets/main.css"
@@ -12,7 +14,7 @@ export default ({ children }) => (
         href="https://cdn.jsdelivr.net/npm/inter-ui@3.10.0/inter.min.css"
       />
     </Helmet>
-
+    <GithubCorner href="https://github.com/deamme/usecrypto" />
     {children}
   </div>
 )
