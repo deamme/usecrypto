f02329fbca1ea59797bb3d009fe76965b5671b6e
Frederik Bolding <frederik.bolding@gmail.com>
2019-10-20 22:48:20 +0200

    Added initial SEO

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -9,6 +9,35 @@ const Layout = ({ children }) => (
   <div className="wrapper">
     <Helmet>
       <meta charSet="utf-8" />
+
+      {/** Primary Meta Tags **/}
+      <title>Use Crypto</title>
+      <meta name="title" content="Use Crypto" />
+      <meta
+        name="description"
+        content="A list of resources where you can spend your ETH, BTC, DAI etc."
+      />
+
+      {/** Open Graph / Facebook **/}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://usecrypto.xyz/" />
+      <meta property="og:title" content="Use Crypto" />
+      <meta
+        property="og:description"
+        content="A list of resources where you can spend your ETH, BTC, DAI etc."
+      />
+      <meta property="og:image" content="https://webshot.deam.io/usecrypto.xyz" />
+
+      {/** Twitter  **/}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://usecrypto.xyz/" />
+      <meta property="twitter:title" content="Use Crypto" />
+      <meta
+        property="twitter:description"
+        content="A list of resources where you can spend your ETH, BTC, DAI etc."
+      />
+      <meta property="twitter:image" content="https://webshot.deam.io/usecrypto.xyz" />
+
       <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/inter-ui@3.10.0/inter.min.css"
@@ -19,4 +48,4 @@ const Layout = ({ children }) => (
   </div>
 )
 
-export default Layout;
+export default Layout
